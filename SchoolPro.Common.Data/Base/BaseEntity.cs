850d1bb4c999bb104b140e0707446bc19794df59
Kayle <Kayle@DESKTOP-020GO53>
2024-07-03 21:10:49 -0400

    Agregar archivos de proyecto.

---
+++ SchoolPro.Common.Data/Base/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolPro.Common.Data.Base
+{
+    internal class BaseEntity
+    {
+    }
+}
